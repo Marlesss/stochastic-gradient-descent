e1bd77b1f4c35f10760faa257e65d2fb171d280e
Marlesss <334874@niuitmo.ru>
2023-04-14 22:01:59 +0300

    modifications completed

--- gradient_descent.py
+++ gradient_descent.py
@@ -3,6 +3,15 @@ from random import shuffle
 from typing import Callable
 import math
 
+EPS = 10 ** -8
+
+
+def constant_learning_rate():
+    def func(init_learning_rate: float, epoch: int):
+        return init_learning_rate
+
+    return func
+
 
 def linear_learning_rate_scheduler(schedule_coef: float, epoch_step: int) -> Callable[[float, int], float]:
     def func(init_learning_rate: float, epoch: int) -> float:
@@ -32,10 +41,161 @@ def stochastic_gradient_descent(dots: np.ndarray, batch_size: int, start_value: 
             minibatch_i = dots_i[i * batch_size: (i + 1) * batch_size]
             minibatch_dots = [dots[i] for i in minibatch_i]
             grad = np.array([0.0, 0.0])
+            #     sum( (dot[1] - (func_arg[0] * dot[0] + func_arg[1])) ** 2 for dot in dots)
             for dot in minibatch_dots:
                 grad[0] += 2 * dot[0] * (dot[0] * prev_args[0] - dot[1] + prev_args[1])
                 grad[1] += 2 * (prev_args[1] - dot[1] + prev_args[0] * dot[0])
             new_args = prev_args - learning_rate * grad
             way.append(new_args)
             prev_args = new_args
+    return np.array(way)
+
+
+def running_average_stochastic_gradient_descent_constant(dots: np.ndarray, batch_size: int,
+                                                         start_value: np.ndarray,
+                                                         learning_rate: float,
+                                                         gamma: float):
+    way = [start_value]
+
+    dots_i = list(range(len(dots)))
+    prev_args = start_value
+    prev_v = np.array([0.0, 0.0])
+    for epoch in range(50):
+        shuffle(dots_i)
+        for i in range((len(dots) + batch_size - 1) // batch_size):
+            minibatch_i = dots_i[i * batch_size: (i + 1) * batch_size]
+            minibatch_dots = [dots[i] for i in minibatch_i]
+            grad = np.array([0.0, 0.0])
+            for dot in minibatch_dots:
+                grad[0] += 2 * dot[0] * (dot[0] * prev_args[0] - dot[1] + prev_args[1])
+                grad[1] += 2 * (prev_args[1] - dot[1] + prev_args[0] * dot[0])
+
+            v = gamma * prev_v + (1 - gamma) * grad
+
+            new_args = prev_args - learning_rate * v
+
+            way.append(new_args)
+            prev_args = new_args
+            prev_v = v
+    return np.array(way)
+
+
+def nesterov_stochastic_gradient_descent_constant(dots: np.ndarray, batch_size: int,
+                                                  start_value: np.ndarray,
+                                                  learning_rate: float,
+                                                  gamma: float):
+    way = [start_value]
+
+    dots_i = list(range(len(dots)))
+    prev_args = start_value
+    prev_v = np.array([0.0, 0.0])
+    for epoch in range(50):
+        shuffle(dots_i)
+        for i in range((len(dots) + batch_size - 1) // batch_size):
+            minibatch_i = dots_i[i * batch_size: (i + 1) * batch_size]
+            minibatch_dots = [dots[i] for i in minibatch_i]
+            grad = np.array([0.0, 0.0])
+            grad_arg = prev_args - learning_rate * gamma * prev_v
+            for dot in minibatch_dots:
+                grad[0] += 2 * dot[0] * (dot[0] * grad_arg[0] - dot[1] + grad_arg[1])
+                grad[1] += 2 * (grad_arg[1] - dot[1] + grad_arg[0] * dot[0])
+
+            v = gamma * prev_v + (1 - gamma) * grad
+
+            new_args = prev_args - learning_rate * v
+
+            way.append(new_args)
+            prev_args = new_args
+            prev_v = v
+    return np.array(way)
+
+
+def adagrad_stochastic_gradient_descent_constant(dots: np.ndarray, batch_size: int,
+                                                 start_value: np.ndarray,
+                                                 learning_rate: float):
+    way = [start_value]
+
+    dots_i = list(range(len(dots)))
+    prev_args = start_value
+    G = np.array([0.0, 0.0])
+    for epoch in range(50):
+        shuffle(dots_i)
+        for i in range((len(dots) + batch_size - 1) // batch_size):
+            minibatch_i = dots_i[i * batch_size: (i + 1) * batch_size]
+            minibatch_dots = [dots[i] for i in minibatch_i]
+            grad = np.array([0.0, 0.0])
+            for dot in minibatch_dots:
+                grad[0] += 2 * dot[0] * (dot[0] * prev_args[0] - dot[1] + prev_args[1])
+                grad[1] += 2 * (prev_args[1] - dot[1] + prev_args[0] * dot[0])
+
+            G += grad ** 2
+
+            new_args = prev_args - learning_rate * ((G + EPS) ** (-1 / 2)) * grad
+
+            way.append(new_args)
+            prev_args = new_args
+    return np.array(way)
+
+
+def rmsprop_stochastic_gradient_descent_constant(dots: np.ndarray, batch_size: int,
+                                                 start_value: np.ndarray,
+                                                 learning_rate: float, beta: float):
+    way = [start_value]
+
+    dots_i = list(range(len(dots)))
+    prev_args = start_value
+    prev_s = np.array([0.0, 0.0])
+    for epoch in range(50):
+        shuffle(dots_i)
+        for i in range((len(dots) + batch_size - 1) // batch_size):
+            minibatch_i = dots_i[i * batch_size: (i + 1) * batch_size]
+            minibatch_dots = [dots[i] for i in minibatch_i]
+            grad = np.array([0.0, 0.0])
+            for dot in minibatch_dots:
+                grad[0] += 2 * dot[0] * (dot[0] * prev_args[0] - dot[1] + prev_args[1])
+                grad[1] += 2 * (prev_args[1] - dot[1] + prev_args[0] * dot[0])
+
+            s = beta * prev_s + (1 - beta) * (grad ** 2)
+
+            new_args = prev_args - learning_rate * grad * ((s + EPS) ** (-1 / 2))
+
+            way.append(new_args)
+            prev_args = new_args
+            prev_s = s
+    return np.array(way)
+
+
+def adam_stochastic_gradient_descent_constant(dots: np.ndarray, batch_size: int,
+                                              start_value: np.ndarray,
+                                              learning_rate: float, beta1: float, beta2: float):
+    way = [start_value]
+
+    dots_i = list(range(len(dots)))
+    prev_args = start_value
+    prev_v = np.array([0.0, 0.0])
+    prev_s = np.array([0.0, 0.0])
+    beta1_powered = beta1
+    beta2_powered = beta2
+    for epoch in range(50):
+        shuffle(dots_i)
+        for i in range((len(dots) + batch_size - 1) // batch_size):
+            minibatch_i = dots_i[i * batch_size: (i + 1) * batch_size]
+            minibatch_dots = [dots[i] for i in minibatch_i]
+            grad = np.array([0.0, 0.0])
+            for dot in minibatch_dots:
+                grad[0] += 2 * dot[0] * (dot[0] * prev_args[0] - dot[1] + prev_args[1])
+                grad[1] += 2 * (prev_args[1] - dot[1] + prev_args[0] * dot[0])
+
+            v = beta1 * prev_v + (1 - beta1) * grad
+            s = beta2 * prev_s + (1 - beta2) * (grad ** 2)
+
+            new_args = (prev_args -
+                        learning_rate * (v / (1 - beta1_powered)) * (s / (1 - beta2_powered) + EPS) ** (-1 / 2))
+
+            way.append(new_args)
+            prev_args = new_args
+            prev_v = v
+            prev_s = s
+            beta1_powered *= beta1
+            beta2_powered *= beta2
     return np.array(way)
--- main.py
+++ main.py
@@ -8,6 +8,23 @@ def linear_regression_mistake(dots: np.ndarray, func_arg: np.ndarray) -> float:
     return sum(abs(dot[1] - (func_arg[0] * dot[0] + func_arg[1])) for dot in dots)
 
 
+def add_solution_to_plot(dots: np.ndarray, way: np.ndarray, show=True):
+    # print(way)
+    ans = way[-1]
+    plt.plot(dots[:, 0], dots[:, 1], 'o')
+    # print(ans)
+    x_space = np.linspace(min(dots[:, 0]), max(dots[:, 0]), 30)
+    plt.plot(x_space, [ans[0] * x + ans[1] for x in x_space])
+    if show:
+        plt.show()
+    mistake = sum(abs(dot[1] - (ans[0] * dot[0] + ans[1])) for dot in dots)
+    print(f"Mistake of solution is {mistake}")
+
+
+def add_way_to_plot(dots: np.ndarray, way: np.ndarray, show=True):
+    show_2arg_func(lambda func_arg: linear_regression_mistake(dots, func_arg), way, show=show)
+
+
 def main():
     dots = np.array([
         [1, 1],
@@ -15,20 +32,14 @@ def main():
         [4, 2],
         [5, 6]
     ])
-    way = stochastic_gradient_descent(dots, 2, np.array([1.0, 0.0]), 0.05,
-                                      exponent_learning_rate_scheduler(0.3))
-    print(way)
-    ans = way[-1]
-    plt.plot(dots[:, 0], dots[:, 1], 'o')
-    print(ans)
-    x_space = np.linspace(min(dots[:, 0]), max(dots[:, 0]), 30)
-    plt.plot(x_space, [ans[0] * x + ans[1] for x in x_space])
-    plt.show()
-    mistake = sum(abs(dot[1] - (ans[0] * dot[0] + ans[1])) for dot in dots)
-    print(f"Mistake of solution is {mistake}")
-
-    # show the way of gradient descent
-    show_2arg_func(lambda func_arg: linear_regression_mistake(dots, func_arg), way)
+    # way = stochastic_gradient_descent(dots, 2, np.array([1.0, 0.0]), 0.01, constant_learning_rate())
+    # way = running_average_stochastic_gradient_descent_constant(dots, 2, np.array([1.0, 0.0]), 0.01, 0.95)
+    # way = nesterov_stochastic_gradient_descent_constant(dots, 2, np.array([1.0, 0.0]), 0.01, 0.95)
+    # way = adagrad_stochastic_gradient_descent_constant(dots, 2, np.array([1.0, 0.0]), 0.3)
+    # way = rmsprop_stochastic_gradient_descent_constant(dots, 2, np.array([1.0, 0.0]), 0.05, 0.99)
+    way = adam_stochastic_gradient_descent_constant(dots, 2, np.array([1.0, 0.0]), 0.05, 0.9, 0.999)
+    add_solution_to_plot(dots, way)
+    add_way_to_plot(dots, way)
 
 
 if __name__ == "__main__":
